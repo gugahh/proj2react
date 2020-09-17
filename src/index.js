bed787b70f5370b7b81b88fa90489ad4cfd54ee1
Gustavo Santos <gugahh.br@gmail.com>
2020-09-16 22:51:12 -0300

    Evolução

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+// Import React and ReactDOC Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
+// Create the React Component
+const App = () => {
+
+	const buttonText = { 
+		description: 'Kenobi', 
+		age: 15,
+		toString : function () {
+			return this.description; 
+		}
+	};
+
+	const style = { 
+		color: 'white', 
+		backgroundColor: 'blue' 
+	};
+
+	return (
+		<div>
+			<label htmlFor="name" className="label">Nome:</label>
+			<input type="text" id="name"/>
+			<button style={style}>
+				{buttonText.toString()}
+			</button>
+		</div>
+	);
+}
+
+// Take the react component and show it on Screen
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App />,
+	document.querySelector('#root')
 );
